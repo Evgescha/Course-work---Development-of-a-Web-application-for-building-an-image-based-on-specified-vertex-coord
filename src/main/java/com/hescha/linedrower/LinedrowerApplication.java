1bf4ed05c1469cf432f8442944010d5c15d4c16b
Yauhescha <evgescha.maslov007@yandex.ru>
2022-11-10 19:12:33 +0300

    Initial commit

---
+++ src/main/java/com/hescha/linedrower/LinedrowerApplication.java
@@ -0,0 +1,13 @@
+package com.hescha.linedrower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinedrowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinedrowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hescha/linedrower/LinedrowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hescha.linedrower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinedrowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
